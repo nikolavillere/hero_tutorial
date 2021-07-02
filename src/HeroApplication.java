21880bdd0ad7587711c4e2a18f422dfe29bd8325
nikolavillere <nikola.villere@gmail.com>
2021-07-02 21:22:05 +0300

    first commit

---
+++ src/HeroApplication.java
@@ -0,0 +1,54 @@
+public class HeroApplication {
+
+    public static void main(String[] args) {
+
+    //Variable initialization
+        String heroName;
+        String herCity;
+
+        String heroPower1, heroPower2, heroPower3;
+        boolean isEvil;
+        double heroSalary;
+
+        String  realName;
+
+        String universe;
+
+    //Variable assignment
+        heroName = "FlowerGirl";
+        herCity = "Riga";
+        heroPower1 = "Spreads flowers";
+        heroPower2 = "Flower breath";
+        heroPower3 = "";
+        isEvil = true;
+        heroSalary = 1500;
+        realName = "Selga";
+        universe = "Floral";
+
+    //Print out hero information card
+
+        System.out.println("****************************");
+        System.out.println("*** HERO INFORMATION ***");
+        System.out.println("****************************");
+
+        System.out.println("Hero name: " + heroName);
+        System.out.println("-----------------------------");
+        System.out.println("This hero lives in: " + herCity);
+        System.out.println("-----------------------------");
+        System.out.println("Hero has superpower: " + heroPower1);
+        System.out.println("Hero has superpower: " + heroPower2);
+        System.out.println("Hero has superpower: " + heroPower3);
+        System.out.println("-----------------------------");
+        System.out.println("This hero is evil: " + isEvil);
+        System.out.println("-----------------------------");
+        System.out.println("Hero earns: " + heroSalary + " EUR");
+        System.out.println("-----------------------------");
+        System.out.println("Real identity: " + realName);
+        System.out.println("-----------------------------");
+        System.out.println("Hero lives in: " + universe + "universe");
+        System.out.println("****************************");
+        System.out.println("*** HERO INFORMATION ***");
+        System.out.println("****************************");
+
+    }
+}
